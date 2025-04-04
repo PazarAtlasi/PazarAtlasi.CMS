38ab63cdf3fc0d97fb66a618e2d9b5bc49a7e0f7
SezginKahraman <sezgin.kahraman12@gmail.com>
2025-04-04 23:41:49 +0300

    .

--- PazarAtlasi.CMS.Application/Features/WebUrls/Commands/DeleteWebUrl/DeleteWebUrlResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace PazarAtlasi.CMS.Application.Features.WebUrls.Commands.DeleteWebUrl;
-
-public class DeleteWebUrlResponse
-{
-    public int Id { get; set; }
-    public bool IsSuccess { get; set; }
-    public required string Message { get; set; }
-}
