f03249a81471b9ec4ca020224cde88af0b006257
SezginKahraman <sezgin.kahraman12@gmail.com>
2025-04-05 20:37:19 +0300

    cache clear.

--- PazarAtlasi.CMS/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace PazarAtlasi.CMS.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string CompanyName { get; set; }
-    }
-} 
--- PazarAtlasi.CMS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PazarAtlasi.CMS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
