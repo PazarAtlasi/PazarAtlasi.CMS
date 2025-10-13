9fde4ddfa083e0b1283754df3c47d4a917c99d1e
SezginKahraman <sezgin.kahraman12@gmail.com>
2025-10-14 01:28:19 +0300

    t.

---
+++ PazarAtlasi.CMS.Persistence/Migrations/20251013222802_t.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PazarAtlasi.CMS.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class t : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SectionItemTranslations_LanguageId",
+                table: "SectionItemTranslations",
+                column: "LanguageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SectionItemTranslations_Languages_LanguageId",
+                table: "SectionItemTranslations",
+                column: "LanguageId",
+                principalTable: "Languages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SectionItemTranslations_Languages_LanguageId",
+                table: "SectionItemTranslations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SectionItemTranslations_LanguageId",
+                table: "SectionItemTranslations");
+        }
+    }
+}
