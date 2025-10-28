71caf585eef455266fd78aa1f0e87aeb8ed28925
SezginKahraman <sezgin.kahraman12@gmail.com>
2025-10-28 14:48:13 +0300

    API is added.

---
+++ PazarAtlasi.API/Controllers/ContentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PazarAtlasi.CMS.Application.Interfaces.Infrastructure;
+using PazarAtlasi.CMS.Persistence.Context;
+
+namespace PazarAtlasi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentController : ControllerBase
+    {
+        private readonly PazarAtlasiDbContext _pazarAtlasiDbContext;
+
+        public ContentController(
+            PazarAtlasiDbContext pazarAtlasiDbContext)
+        {
+            _pazarAtlasiDbContext = pazarAtlasiDbContext;
+        }
+    }
+}
